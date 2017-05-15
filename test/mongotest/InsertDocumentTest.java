f1628905b86999d5f15af307ba1de83d639a8786
Gustavo <Gustavo@Gustavo>
2017-05-15 00:34:04 -0300

    Inserção de documentos.

---
+++ test/mongotest/InsertDocumentTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mongotest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class InsertDocumentTest {
+    
+    public InsertDocumentTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of insert method, of class InsertDocument.
+     */
+    @Test
+    public void testInsert() {
+        System.out.println("insert");
+        InsertDocument instance = null;
+        instance.insert();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
