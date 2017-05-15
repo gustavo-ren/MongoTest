82c33e58c4529638a2ab48a6e371e3f753e04bbb
Gustavo <Gustavo@Gustavo>
2017-05-15 00:29:07 -0300

    Inserção de documentos.

--- src/mongotest/MongoTest.java
+++ src/mongotest/MongoTest.java
@@ -10,26 +10,6 @@ public class MongoTest {
 
     public static void main(String[] args) throws Exception {
 
-        UpdateDocument document=new UpdateDocument();
-        document.updateSelected("ly");
-//        Scanner in=new Scanner(System.in);
-//        
-//        System.out.print("ID: ");
-//        String id=in.nextLine();
-//        
-//        System.out.print("Name: ");
-//        String name=in.nextLine();
-//        
-//        System.out.print("Zanpakutou: ");
-//        String zanpakutou=in.nextLine();
-//        
-//        InsertDocument insertDocument=new InsertDocument(id, name, zanpakutou);
-//        
-//        insertDocument.insert();
-//        
-//        SearchDocument searchDocument=new SearchDocument();
-//        searchDocument.selectAllRecordsFromACollection("ly");
-
     }
 
 }
